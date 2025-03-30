1ae938a2721b6c0c5d21998cd6effa160a1ae8b9
Nick Sanford <nicholascsanford@gmail.com>
2025-03-30 16:37:49 -0400

    get drag and drop working with leaks

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,54 @@
 const std = @import("std");
 const c = @cImport(@cInclude("raylib.h"));
-pub fn main() void {
+pub fn main() !void {
     const width = 800;
     const height = 450;
     const fps = 60;
-    c.InitWindow(width, height, "example [basic window]");
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer std.debug.assert(gpa.deinit() == .ok);
+
+    var list = std.ArrayList([]u8).init(allocator);
+    defer list.deinit();
+
+    c.InitWindow(width, height, "drop files");
     c.SetTargetFPS(fps);
+    // std.ArrayList(comptime T: type)
     while (!c.WindowShouldClose()) {
+        // UPDATE
+        // ---
+        if (c.IsFileDropped()) {
+            const droppedFiles = c.LoadDroppedFiles();
+            defer c.UnloadDroppedFiles(droppedFiles);
+
+            for (0..droppedFiles.count) |i| {
+                const x = try allocator.dupeZ(u8, std.mem.span(droppedFiles.paths[i]));
+                // const x = try allocator.dupeZ();
+                try list.append(x);
+            }
+        }
+        // ---
+
+        // DRAW
+        // ---
         c.BeginDrawing();
+
         c.ClearBackground(c.RAYWHITE);
-        c.DrawText("Congrats! You made your first window!!!", 190, 200, 20, c.LIGHTGRAY);
+        if (list.items.len == 0) {
+            c.DrawText("Drop files into the window!!!", 100, 40, 20, c.DARKGRAY);
+        } else {
+            c.DrawText("Dropped files:", 100, 40, 20, c.DARKGRAY);
+            for (0.., list.items) |i, file| {
+                const fademod: f32 = if (i % 2 == 0) 0.5 else 0.3;
+                c.DrawRectangle(0, @as(c_int, @intCast(85 + 40 * i)), width, height, c.Fade(c.LIGHTGRAY, fademod));
+                c.DrawText(file.ptr, 120, @as(c_int, @intCast(100 + 40 * i)), 10, c.GRAY);
+            }
+            c.DrawText("Drop new files...", 100, @as(c_int, @intCast(110 + 40 * list.items.len)), 20, c.DARKGRAY);
+        }
+
         c.EndDrawing();
+        // ---
     }
 
     c.CloseWindow();
